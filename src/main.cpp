7209ae827a01a2d2c71b0954ba5150857c133c7a
joppe <jboeve009@gmail.com>
2026-03-19 20:18:13 +0100

    Add display_config.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+// lvgl8 en snel
+
+#include "display_config.hpp"
+
+
 #include <Wire.h>
 #define LGFX_USE_V1
 #include <LovyanGFX.hpp>
@@ -6,88 +11,7 @@
 
 int screen_brightness = 255;
 
-class LGFX : public lgfx::LGFX_Device
-{
-  lgfx::Panel_ST7796 _panel_instance;
-  lgfx::Bus_Parallel8 _bus_instance;
-  lgfx::Light_PWM _light_instance;
-  lgfx::Touch_FT5x06 _touch_instance;
-
-public:
-  LGFX(void)
-  {
-    {
-      auto cfg = _bus_instance.config();
-      cfg.freq_write = 40000000;
-      cfg.pin_wr = 47;
-      cfg.pin_rd = -1;
-      cfg.pin_rs = 0;
-      cfg.pin_d0 = 9;
-      cfg.pin_d1 = 46;
-      cfg.pin_d2 = 3;
-      cfg.pin_d3 = 8;
-      cfg.pin_d4 = 18;
-      cfg.pin_d5 = 17;
-      cfg.pin_d6 = 16;
-      cfg.pin_d7 = 15;
-      _bus_instance.config(cfg);
-      _panel_instance.setBus(&_bus_instance);
-    }
-
-    {
-      auto cfg = _panel_instance.config();
-      cfg.pin_cs = -1;
-      cfg.pin_rst = 4;
-      cfg.pin_busy = -1;
-      cfg.memory_width = 320;
-      cfg.memory_height = 480;
-      cfg.panel_width = 320;
-      cfg.panel_height = 480;
-      cfg.offset_x = 0;
-      cfg.offset_y = 0;
-      cfg.offset_rotation = 0;
-      cfg.dummy_read_pixel = 8;
-      cfg.dummy_read_bits = 1;
-      cfg.readable = true;
-      cfg.invert = true;
-      cfg.rgb_order = false;
-      cfg.dlen_16bit = false;
-      cfg.bus_shared = true;
-
-      _panel_instance.config(cfg);
-    }
-
-    {
-      auto cfg = _light_instance.config();
-      cfg.pin_bl = 45;
-      cfg.invert = false;
-      cfg.freq = 44100;
-      cfg.pwm_channel = 7;
-
-      _light_instance.config(cfg);
-      _panel_instance.setLight(&_light_instance);
-    }
-
-    {
-      auto cfg = _touch_instance.config();
-      cfg.i2c_port = 1;
-      cfg.i2c_addr = 0x38;
-      cfg.pin_sda = 6;
-      cfg.pin_scl = 5;
-      cfg.freq = 400000;
-      cfg.x_min = 0;
-      cfg.x_max = 320;
-      cfg.y_min = 0;
-      cfg.y_max = 480;
-
-      _touch_instance.config(cfg);
-      _panel_instance.setTouch(&_touch_instance);
-    }
-
-    setPanel(&_panel_instance);
-  }
-};
-static LGFX tft;
+static LGFX lcd;
 
 /*Change to your screen resolution*/
 static const uint32_t screenWidth = 480;
@@ -101,10 +25,10 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
 
-  tft.startWrite();
-  tft.setAddrWindow(area->x1, area->y1, w, h);
-  tft.writePixels((lgfx::rgb565_t *)&color_p->full, w * h);
-  tft.endWrite();
+  lcd.startWrite();
+  lcd.setAddrWindow(area->x1, area->y1, w, h);
+  lcd.writePixels((lgfx::rgb565_t *)&color_p->full, w * h);
+  lcd.endWrite();
 
   lv_disp_flush_ready(disp);
 }
@@ -114,7 +38,7 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
 
   uint16_t x, y;
-  if (tft.getTouch(&x, &y))
+  if (lcd.getTouch(&x, &y))
   {
     data->state = LV_INDEV_STATE_PR;
     data->point.x = x;
@@ -128,10 +52,11 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 
 void initDisplay()
 {
-  tft.begin();
-  tft.setRotation(1);
-  tft.setBrightness(screen_brightness);
-  tft.fillScreen(TFT_BLACK);
+  lcd.begin();
+  lcd.setRotation(3);
+  lcd.setBrightness(screen_brightness);
+  lcd.fillScreen(TFT_BLACK);
+
 }
 
 
@@ -140,6 +65,7 @@ void setup()
   Serial.begin(115200);
   initDisplay();
   lv_init();
+  // @param size_in_px_cnt size of the `buf1` and `buf2` in pixel count.
   lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * 100);
 
   /*Initialize the display*/
@@ -159,7 +85,7 @@ void setup()
   indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
 
-  tft.setBrightness(255);
+  lcd.setBrightness(255);
 
   lv_demo_widgets();
 }
